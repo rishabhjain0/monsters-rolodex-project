f4700af099f20692cd6b49bf2bbccd836f9215c0
rishabhjain0 <jainrishabh7899@gmail.com>
2020-06-12 17:41:03 +0530

    Updates

---
+++ precache-manifest.8988be1d59174ff5c60979de8c508619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e6c35cb099927ee2284770dd51306c",
+    "url": "/monsters-rolodex-project/index.html"
+  },
+  {
+    "revision": "652ad87b9cb7614f7508",
+    "url": "/monsters-rolodex-project/static/css/main.cae09095.chunk.css"
+  },
+  {
+    "revision": "95bef5d1f0b8c893b684",
+    "url": "/monsters-rolodex-project/static/js/2.9818ad5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-project/static/js/2.9818ad5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652ad87b9cb7614f7508",
+    "url": "/monsters-rolodex-project/static/js/main.f8df5cb0.chunk.js"
+  },
+  {
+    "revision": "e0565d98d9c71736bcfd",
+    "url": "/monsters-rolodex-project/static/js/runtime-main.50ef05be.js"
+  }
+]);
